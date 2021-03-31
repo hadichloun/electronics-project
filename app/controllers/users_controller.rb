617130c00f05967d8005979c3b7c376c296c04db
joey <pball626@gmail.com>
2021-03-31 10:09:06 -0500

    updates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,12 +9,22 @@ class UsersController < ApplicationController
 
     def create 
         # byebug
+        # create an instance of user
         @user = User.new(user_params)
-        # if @user.valid_name?
-        session[:username] = @user.username
-        session[:password] = @user.password
-        # else
-        redirect_to '/users/:id'
+            # check if valid
+        if @user.valid? 
+            # if true
+            # create session
+            @user.save
+            session[:id] = @user.id
+            redirect_to @user
+            # if false
+        else
+            # show errors
+            flash[:errors] = @user.errors.full_messages
+            # come back to form
+            redirect_to new_user_path
+        end
     end
 
     def show
@@ -33,7 +43,7 @@ class UsersController < ApplicationController
 
     private
     def user_params
-        params.require(:user).permit(:username, :password)
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 end
 

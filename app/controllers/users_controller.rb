42500518804a5d23be60988241577dd460cd3e26
joey <pball626@gmail.com>
2021-03-31 09:53:49 -0500

    changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+    before_action :current_user, only: [:show]
+    before_action :not_logged_in, only: [:show] 
+    
     def index
         @user = User.all
     end
